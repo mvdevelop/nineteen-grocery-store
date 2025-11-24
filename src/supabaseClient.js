d1ced3259a936cccba21e9cfc99dc4025066d304
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-11-24 09:52:37 -0300

    reseting supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pyeefbssdgedhifudsar.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZWVmYnNzZGdlZGhpZnVkc2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMzgzMTMsImV4cCI6MjA2NTkxNDMxM30.-1CNoKdasGpTud1feKiYknrCvhxJTJ7aKuQXn7L5BAs";
+const supabaseUrl = "https://inphfapjnmfedemyntlq.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlucGhmYXBqbm1mZWRlbXludGxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NjU1NjksImV4cCI6MjA3OTU0MTU2OX0.O4Nsi9QzpQJvQN5zEbF17upGfN-melPPsQBBUIHSprg";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
